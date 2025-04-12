94aa3b5bdc16011c8c69314f9876fd2e5f972f7a
Israel Imoh <izykiytonez@gmail.com>
2025-04-12 16:27:30 +0100

    newone

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,15 +16,22 @@ const Navbar = () => {
       <div className="navbar-right">
         <ul className="nav-links">
           {/* f */}
-          <li className="link-item-1">
-            Book a demo{" "}
-            <span>
-              <RiArrowRightFill size={16} style={{ marginBottom: "-3.5px" }} />
-            </span>
-          </li>
-          <li className="link-item-2">
-            Start free trial <img src={Umbrella_icon} alt="" />{" "}
-          </li>
+          <a href="https://cal.com/ruut-csm" target="_blank">
+            <li className="link-item-1">
+              Book a demo{" "}
+              <span>
+                <RiArrowRightFill
+                  size={16}
+                  style={{ marginBottom: "-3.5px" }}
+                />
+              </span>
+            </li>
+          </a>
+          <a href="https://app.ruut.chat/app/login" target="_blank">
+            <li className="link-item-2">
+              Start free trial <img src={Umbrella_icon} alt="" />{" "}
+            </li>
+          </a>
         </ul>
       </div>
     </div>
